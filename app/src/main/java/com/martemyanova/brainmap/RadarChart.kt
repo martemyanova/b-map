46c4e4462c4cad73c7928a225184882bc465a2aa
martemyanova <martemyanova.julia@gmail.com>
2018-03-01 13:57:32 +0000

    Position labels with values.

--- app/src/main/java/com/martemyanova/brainmap/RadarChart.kt
+++ app/src/main/java/com/martemyanova/brainmap/RadarChart.kt
@@ -2,16 +2,15 @@ package com.martemyanova.brainmap
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Typeface
 import android.support.v4.content.ContextCompat
-import android.util.AttributeSet
-import android.view.View
-import android.util.TypedValue
 import android.text.Layout
 import android.text.StaticLayout
 import android.text.TextPaint
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.View
 import java.util.*
 
 
@@ -24,6 +23,14 @@ class RadarChart : View {
     private val axisCircleRadius = (11F / 2).dpToPx()
     private val paint = Paint()
     private val textPaint = TextPaint()
+    private val textSize = 14F.spToPx()
+    private val textHeight = 10F.dpToPx()
+    private val sideBorder = 10F.dpToPx()
+
+    private val backgroundColor by lazy { ContextCompat.getColor(context, R.color.colorGrey) }
+    private val greyColor by lazy { ContextCompat.getColor(context, R.color.colorLightGrey) }
+    private val whiteColor by lazy { ContextCompat.getColor(context, R.color.colorWhite) }
+    private val blueColor by lazy { ContextCompat.getColor(context, R.color.colorBlue) }
 
     constructor(context: Context) : super(context)
 
@@ -33,11 +40,11 @@ class RadarChart : View {
             super(context, attrs, defStyleAttr)
 
     init {
-        setBackgroundColor(ContextCompat.getColor(context, R.color.colorGrey))
+        setBackgroundColor(backgroundColor)
 
         paint.isAntiAlias = true
-        textPaint.color = Color.WHITE
-        textPaint.textSize = 50F
+        textPaint.color = whiteColor
+        textPaint.textSize = textSize
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -46,65 +53,90 @@ class RadarChart : View {
         c.translate(width/2f, height/2f);
         drawCircles(c)
         drawAxis(c)
-
-
-        //c.drawLine(0F, 0F, x, y, paint);
-        //c.drawLine(x, 0F, 0F, y, paint);
-
     }
 
     private fun drawCircles(canvas: Canvas) {
-        paint.color = Color.WHITE
+        paint.color = whiteColor
         paint.style = Paint.Style.STROKE
 
         val h = outerCircleRadius / numberOfCircles
 
         for (i in 1..numberOfCircles) {
-            if (i % 2 == 0) paint.color = Color.WHITE
-            else paint.color = ContextCompat.getColor(context, R.color.colorLightGrey)
+            if (i % 2 == 0) paint.color = whiteColor
+            else paint.color = greyColor
             canvas.drawCircle(0F, 0F, i * h, paint);
         }
     }
 
     private fun drawAxis(c: Canvas) {
-        paint.color = Color.WHITE
         val r = axisHeight
+        val circleH = r + axisCircleRadius
 
         for (i in 0 until numberOfAxis) {
-            val alfa = Math.toRadians((i * 360 / numberOfAxis - 90).toDouble())
-            var x = Math.cos(alfa).toFloat()
-            var y = Math.sin(alfa).toFloat()
+            val x = getXUnitVector(i)
+            val y = getYUnitVector(i)
+            paint.color = whiteColor
             c.drawLine(0F, 0F, r * x, r * y, paint);
 
-            val circleH = r + axisCircleRadius
+            paint.color = blueColor
             c.drawCircle(circleH * x, circleH * y, axisCircleRadius, paint);
 
-            drawText(c, "Text $i", circleH * x, circleH * y)
+            drawText(c, "Peak Brain Score", "${i}00", circleH * x, circleH * y)
         }
     }
 
-    private fun drawText(c: Canvas, text: String, x: Float, y: Float) {
+    private fun drawText(c: Canvas, text: String, value: String, x: Float, y: Float) {
+        val typeface = Typeface.createFromAsset(context.applicationContext.assets,
+                String.format(Locale.US,"fonts/%s", "GothamSSm-Light.ttf"))
+        val boldTypeface = Typeface.create(typeface, Typeface.BOLD)
+
+        val labelWidth: Int = (((c.width - sideBorder)/2 - Math.abs(x)) * 2).toInt()
+
+        val labelLayout = StaticLayout(text, textPaint, labelWidth,
+                Layout.Alignment.ALIGN_CENTER, 1f, 1f, false)
+        val valueLayout = StaticLayout(value, textPaint, labelWidth,
+                Layout.Alignment.ALIGN_CENTER, 1f, 1f, false)
+
+        val xBias: Float = (labelWidth/2).toFloat()
+        val yBias: Float =
+                if (y <= 0) {
+                    - axisCircleRadius - textHeight - valueLayout.height
+                }
+                else {
+                    textHeight + labelLayout.height
+                }
+
+        //draw value
         c.save()
+        c.translate(x - xBias, y + yBias)
+        textPaint.typeface = boldTypeface
+        valueLayout.draw(c)
+        c.restore()
 
-        var textY =
-        if (y <= 0) y - 4*10F.dpToPx()
-        else y
-
-        c.translate(x - c.width / 2, textY);
-        val sl = StaticLayout(text, textPaint, c.width,
-                Layout.Alignment.ALIGN_CENTER, 1f, 1f, true)
-
-        val am = context.applicationContext.assets
-        val typeface = Typeface.createFromAsset(am, String.format(Locale.US,
-                "fonts/%s", "GothamSSm-Light.ttf"))
+        //draw label
+        c.save()
+        c.translate(x - xBias, y + yBias - labelLayout.height)
         textPaint.typeface = typeface
-        sl.draw(c)
+        labelLayout.draw(c)
         c.restore()
     }
 
-    fun Float.dpToPx(): Float {
-        val metrics = context.resources.displayMetrics
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, metrics)
+    private fun getAngle(position: Int): Double =
+        Math.toRadians((position * 360 / numberOfAxis - 90).toDouble())
+
+    private fun getXUnitVector(position: Int): Float =
+        Math.cos(getAngle(position)).toFloat()
+
+    private fun getYUnitVector(position: Int): Float =
+        Math.sin(getAngle(position)).toFloat()
+
+    private fun Float.dpToPx(): Float {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this,
+                context.resources.displayMetrics)
     }
 
+    private fun Float.spToPx(): Float {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, this,
+                context.resources.displayMetrics)
+    }
 }
