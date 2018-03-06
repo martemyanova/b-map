0802568f386b56822d208c0752329de08beb7a2d
martemyanova <martemyanova.julia@gmail.com>
2018-03-06 13:14:10 +0000

    Refactoring: RadarChart.

--- app/src/main/java/com/martemyanova/brainmap/RadarChart.kt
+++ app/src/main/java/com/martemyanova/brainmap/RadarChart.kt
@@ -17,7 +17,7 @@ class RadarChart : View {
     private val numberOfCircles = 10
     private val strokeWidth1 = 3F
     private val strokeWidth2 = 3F
-    private val axisCircleRadius = (11F / 2).dpToPx()
+    private val axisMarkerRadius = (11F / 2).dpToPx()
     private val paint = Paint()
     private val textPaint = TextPaint()
     private val textSize = 15F.spToPx()
@@ -29,28 +29,27 @@ class RadarChart : View {
         get() = this.height / 2
     private val Canvas.outerCircleRadius: Float
         get() = (this.areaSize * 0.60).toFloat()
-    private val Canvas.axisHeight: Float
+    private val Canvas.axisLength: Float
         get() = (this.outerCircleRadius + this.areaSize * 0.06).toFloat()
 
-    private val lightGreyColor by lazy { ContextCompat.getColor(context, R.color.colorLightGrey) }
-    private val whiteColor by lazy { ContextCompat.getColor(context, R.color.colorWhite) }
     private val textColor by lazy { ContextCompat.getColor(context, R.color.colorText) }
+    private val brightTextColor by lazy { ContextCompat.getColor(context, R.color.colorBrightText) }
     private val lineColor by lazy { ContextCompat.getColor(context, R.color.colorLine) }
+    private val secondLineColor by lazy { ContextCompat.getColor(context, R.color.colorSecondLine) }
     private val firstShapeColor by lazy { ContextCompat.getColor(context, R.color.colorFirstShape) }
     private val secondShapeColor by lazy { ContextCompat.getColor(context, R.color.colorSecondShape) }
+
+    private val markerPalette: Array<Int>
     private val peakBlueColor by lazy { ContextCompat.getColor(context, R.color.colorPeakBlue) }
     private val pinkColor by lazy { ContextCompat.getColor(context, R.color.colorPink) }
     private val greenColor by lazy { ContextCompat.getColor(context, R.color.colorGreen) }
     private val purpleColor by lazy { ContextCompat.getColor(context, R.color.colorPurple) }
     private val orangeColor by lazy { ContextCompat.getColor(context, R.color.colorOrange) }
     private val blueColor by lazy { ContextCompat.getColor(context, R.color.colorBlue) }
-    private val palette: Array<Int>
 
     private var axisLabelIds = arrayOf<Int>()
-    private val numberOfAxis
-        get() = axisLabelIds.size
-    private var data1 = arrayOf<Int>()
-    private var data2 = arrayOf<Int>()
+    private var firstShapeData = arrayOf<Int>()
+    private var secondShapeData = arrayOf<Int>()
 
     var typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL)
 
@@ -63,62 +62,64 @@ class RadarChart : View {
 
     init {
         paint.isAntiAlias = true
-        textPaint.color = whiteColor
         textPaint.textSize = textSize
 
-        palette = arrayOf(peakBlueColor, orangeColor, greenColor, purpleColor, blueColor, pinkColor)
+        markerPalette = arrayOf(peakBlueColor, orangeColor, greenColor, purpleColor, blueColor, pinkColor)
     }
 
-    fun setData(axisLabelIds: Array<Int>, data1: Array<Int>, data2: Array<Int>) {
+    fun setData(axisLabelIds: Array<Int>, firstShapeData: Array<Int>, secondShapeData: Array<Int>) {
         this.axisLabelIds = axisLabelIds
-        this.data1 = data1
-        this.data2 = data2
+        this.firstShapeData = firstShapeData
+        this.secondShapeData = secondShapeData
         invalidate()
     }
 
     override fun onDraw(canvas: Canvas?) {
-        val c = canvas ?: return
+        canvas ?: return
 
-        c.translate(width/2f, height/2f);
-        drawCircles(c)
-        drawAxis(c)
-        drawData(c)
+        canvas.translate(width/2f, height/2f);
+        drawCircles(canvas)
+        drawAxis(canvas)
+        drawShapes(canvas)
     }
 
     private fun drawCircles(canvas: Canvas) {
-        paint.color = whiteColor
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = strokeWidth1
 
         val h = canvas.outerCircleRadius / numberOfCircles
 
         for (i in 1..numberOfCircles) {
-            if (i % 2 == 0) paint.color = lineColor
-            else paint.color = lightGreyColor
+            paint.color = if (i % 2 == 0) lineColor else secondLineColor
             canvas.drawCircle(0F, 0F, i * h, paint);
         }
     }
 
-    private fun drawAxis(c: Canvas) {
+    private fun drawAxis(canvas: Canvas) {
         if (axisLabelIds.isEmpty()) return
 
-        val r = c.axisHeight
-        val circleH = r + axisCircleRadius
-
         for (i in 0 until axisLabelIds.size) {
-            val x = getXUnitVector(i)
-            val y = getYUnitVector(i)
-            paint.color = lineColor
-            paint.strokeWidth = strokeWidth1
-            c.drawLine(0F, 0F, r * x, r * y, paint);
-
-            paint.color = getColor(i)
-            paint.strokeWidth = strokeWidth2
-            c.drawCircle(circleH * x, circleH * y, axisCircleRadius, paint);
-
-            if (data1.isNotEmpty())
-                drawText(c, axisLabelIds[i].idToString(), data1[i], circleH * x, circleH * y)
+            drawAxisLine(canvas, position = i)
+            drawMarker(canvas, position = i)
+            drawText(canvas, position = i)
         }
+    }
+
+    private fun drawAxisLine(canvas: Canvas, position: Int) {
+        paint.color = lineColor
+        paint.strokeWidth = strokeWidth1
+
+        val (x, y) = vectorAt(position, length = canvas.axisLength)
+        canvas.drawLine(0F, 0F, x, y, paint);
+    }
+
+    private fun drawMarker(canvas: Canvas, position: Int) {
+        paint.color = markerColorAt(position)
+        paint.strokeWidth = strokeWidth2
+
+        val markerHeight = canvas.axisLength + axisMarkerRadius
+        val (x, y) = vectorAt(position, length = markerHeight)
+        canvas.drawCircle(x, y, axisMarkerRadius, paint);
     }
 
     private fun Int.idToString(): String = context.getString(this)
@@ -127,80 +128,92 @@ class RadarChart : View {
             StaticLayout(text, textPaint, width, Layout.Alignment.ALIGN_CENTER,
                     1f, 1f, false)
 
-    private fun drawText(c: Canvas, text: String, value: Int, x: Float, y: Float) {
-        val labelWidth: Int = (((c.width - sideBorder)/2 - Math.abs(x)) * 2).toInt()
+    private fun drawText(canvas: Canvas, position: Int) {
+        if (firstShapeData.isEmpty()) return
+
+        val text = axisLabelIds[position].idToString()
+        val value = firstShapeData[position]
+        val markerHeight = canvas.axisLength + axisMarkerRadius
+        val (x, y) = vectorAt(position, markerHeight)
+        val labelWidth: Int = (((canvas.width - sideBorder)/2 - Math.abs(x)) * 2).toInt()
         val labelLayout = createStaticLayout(text, labelWidth)
         val valueLayout = createStaticLayout(value.toString(), labelWidth)
 
         val xBias: Float = (labelWidth/2).toFloat()
         val yBias: Float =
                 if (y <= 1) {
-                    - axisCircleRadius - textHeight - valueLayout.height
+                    - axisMarkerRadius - textHeight - valueLayout.height
                 }
                 else {
                     textHeight + labelLayout.height
                 }
 
         //draw value
-        c.save()
-        c.translate(x - xBias, y + yBias)
+        canvas.save()
+        canvas.translate(x - xBias, y + yBias)
         textPaint.typeface = typeface.setBold()
-        textPaint.color = whiteColor
-        valueLayout.draw(c)
-        c.restore()
+        textPaint.color = brightTextColor
+        valueLayout.draw(canvas)
+        canvas.restore()
 
         //draw label
-        c.save()
-        c.translate(x - xBias, y + yBias - labelLayout.height)
+        canvas.save()
+        canvas.translate(x - xBias, y + yBias - labelLayout.height)
         textPaint.typeface = typeface
         textPaint.color = textColor
-        labelLayout.draw(c)
-        c.restore()
+        labelLayout.draw(canvas)
+        canvas.restore()
     }
 
-    private fun drawData(c: Canvas) {
-        if (data1.isEmpty()) return
+    private fun drawShapes(c: Canvas) {
+        if (firstShapeData.isEmpty()) return
 
-        if (data2.isNotEmpty()) {
+        if (secondShapeData.isNotEmpty()) {
             paint.color = secondShapeColor
-            drawPolygon(c, data2)
+            drawPolygon(c, secondShapeData)
         }
 
         paint.color = firstShapeColor
-        drawPolygon(c, data1)
+        drawPolygon(c, firstShapeData)
     }
 
-    private fun drawPolygon(c: Canvas, data:  Array<Int>) {
+    private fun drawPolygon(canvas: Canvas, data:  Array<Int>) {
         paint.style = Paint.Style.FILL
 
-        val startX = getXUnitVector(0) * data[0].normalize(c.outerCircleRadius)
-        val startY = getYUnitVector(0) * data[0].normalize(c.outerCircleRadius)
+        val (startX, startY) = canvas.normalizedVectorAt(0, data[0].toFloat())
 
         val path = Path()
         path.reset()
         path.moveTo(startX, startY)
 
-        for (i in 1 until numberOfAxis) {
-            val x = getXUnitVector(i) * data[i].normalize(c.outerCircleRadius)
-            val y = getYUnitVector(i) * data[i].normalize(c.outerCircleRadius)
+        for (i in 1 until axisLabelIds.size) {
+            val (x, y) = canvas.normalizedVectorAt(i, data[i].toFloat())
             path.lineTo(x, y)
         }
         path.lineTo(startX, startY)
-        c.drawPath(path, paint)
+        canvas.drawPath(path, paint)
     }
 
-    private fun Int.normalize(outerCircleRadius: Float): Float = outerCircleRadius * this / units
+    private fun Float.normalize(outerCircleRadius: Float): Float = outerCircleRadius * this / units
 
-    private fun getAngle(position: Int): Double =
-        Math.toRadians((position * 360 / numberOfAxis - 90).toDouble())
+    private fun angleAt(position: Int): Double =
+        Math.toRadians((position * 360 / axisLabelIds.size - 90).toDouble())
 
-    private fun getXUnitVector(position: Int): Float =
-        Math.cos(getAngle(position)).toFloat()
+    private fun unitVectorAt(position: Int): Pair<Float, Float> = Pair(
+            Math.cos(angleAt(position)).toFloat(),
+            Math.sin(angleAt(position)).toFloat())
 
-    private fun getYUnitVector(position: Int): Float =
-        Math.sin(getAngle(position)).toFloat()
+    private fun vectorAt(position: Int, length: Float): Pair<Float, Float> {
+        var (x, y) = unitVectorAt(position)
+        x *= length
+        y *= length
+        return Pair(x, y)
+    }
 
-    private fun getColor(position: Int) = palette[position % palette.size]
+    private fun Canvas.normalizedVectorAt(position: Int, length: Float): Pair<Float, Float> =
+            vectorAt(position, length.normalize(this.outerCircleRadius))
+
+    private fun markerColorAt(position: Int) = markerPalette[position % markerPalette.size]
 
     private fun Float.dpToPx(): Float {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this,
